79d358b3e296c00e594b7105208d8f2136bd268c
AniketSarawgi <99114866+AniketSarawgi@users.noreply.github.com>
2025-06-09 15:26:35 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,8 +2,8 @@ import mysql.connector # type: ignore
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",       
-        password="angrybirds",   
-        database="mavericks_training"
+        host="l****",
+        user="****",       
+        password="****",   
+        database="****"
     )
