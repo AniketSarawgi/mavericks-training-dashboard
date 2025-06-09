d8e2f9c53c4a2afef34b440a1ebc91a668231f6a
Raghul <raghulstar1302@gmail.com>
2025-06-09 15:21:10 +0530

    Delete db_config.py

--- db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector # type: ignore
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",       
-        password="angrybirds",   
-        database="mavericks_training"
-    )
